2bf83b9bdb45f2dd329a143d9efe2c46b5d6842d
ahariharan <ahariharan@berkeley.edu>
2025-10-11 20:45:52 -0700

    fix CI new

--- projects/concord/app/models.py
+++
@@ -1,3 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Enum, UniqueConstraint, text, DateTime
-from projects.concord.app.common.types import Priority
