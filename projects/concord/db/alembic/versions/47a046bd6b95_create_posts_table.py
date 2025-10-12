bd0a972c111448b03c39cad5649f1baa1b6aaa14
ahariharan <ahariharan@berkeley.edu>
2025-10-12 01:38:04 -0700

    Setup Alembic for Concord

---
+++ projects/concord/db/alembic/versions/47a046bd6b95_create_posts_table.py
@@ -0,0 +1,26 @@
+"""create posts table
+
+Revision ID: 47a046bd6b95
+Revises: 
+Create Date: 2025-10-12 01:33:48.253586
+
+"""
+from typing import Sequence, Union
+
+
+
+# revision identifiers, used by Alembic.
+revision: str = '47a046bd6b95'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
